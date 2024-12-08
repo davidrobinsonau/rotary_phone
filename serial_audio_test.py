99e653fa53c4cb1f9e65c53da01ca077e4448152
nwtas <115685597+nwtas@users.noreply.github.com>
2024-12-08 20:03:47 +1100

    Adding serial

--- serial_audio_test.py
+++ serial_audio_test.py
@@ -36,9 +36,9 @@ def main():
     print(f"Received: {ser.readline().decode()}")
 
     # Send the AT command to play the file
-    ser.write(AT_PLAY.encode() + "1.WAV".encode() + AT_END.encode())
+    ser.write(AT_PLAY.encode() + "1.wav".encode() + AT_END.encode())
     time.sleep(1)
-    print(f"Sent: {AT_PLAY}1.WAV")
+    print(f"Sent: {AT_PLAY}1.wav")
     print(f"Received: {ser.readline().decode()}")
     time.sleep(5)
 
